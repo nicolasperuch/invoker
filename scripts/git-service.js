ad9f6b1ca7fd21d012f52ee333cf45f6fc168eab
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2018-09-22 16:09:47 -0300

    Adding copy model to git dialog

--- scripts/git-service.js
+++ scripts/git-service.js
@@ -1,7 +1,17 @@
+const shell = require('shelljs');
+
 module.exports = function(robot){
     
     robot.respond(/git (.*)/i, function(res){
         let answer = res.match[1]
         res.send('git url: '+answer)
+    });
+
+    robot.respond(/git copy jenkinsfile to (.*)/i, function(res){
+        let repository = res.match[1]
+        res.send('git url: ' + repository)
+        shell.cd('../shell')
+        shell.exec('./git.sh ' + repository)
+        res.send('done :)')
     });
 }
